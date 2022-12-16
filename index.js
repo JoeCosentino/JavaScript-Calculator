193f09ad3e5568527cc956f8d417fd5678d2e741
Joey Cos <joeycosentino24@gmail.com>
2022-12-16 18:11:15 -0500

    began javascript, starting with selector tags

---
+++ index.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number');
+const operationButtons = document.querySelectorAll('[data-operation');
+const allClearButton = document.querySelector('[data-all-clear');
+const equalsButton = document.querySelector('[data-equals');
+const deleteButton = document.querySelector('[data-delete');
+const previousScreen = document.querySelector('[data-previous');
+const currentScreen = document.querySelector('[data-current');
