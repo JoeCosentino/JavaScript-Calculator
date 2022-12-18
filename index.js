f36b74831bbf394a3eb4db870993f4044da8a77c
Joey Cos <joeycosentino24@gmail.com>
2022-12-18 16:02:47 -0500

    creating class to manage all of the calculators functions

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
-const numberButtons = document.querySelectorAll('[data-number');
-const operationButtons = document.querySelectorAll('[data-operation');
-const allClearButton = document.querySelector('[data-all-clear');
-const equalsButton = document.querySelector('[data-equals');
-const deleteButton = document.querySelector('[data-delete');
-const previousScreen = document.querySelector('[data-previous');
-const currentScreen = document.querySelector('[data-current');
+class Calculator {
+    constructor(previousScreen, currentScreen) {
+        this.previousScreen = previousScreen;
+        this.currentScreen = currentScreen;
+    }
+
+    clear() {
+        this.currentScreen = '';
+        this.previousScreen = '';
+        this.operation = undefined;
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+
+    }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const previousScreen = document.querySelector('[data-previous]');
+const currentScreen = document.querySelector('[data-current]');
