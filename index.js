8f590081dabe0926be2002bb37f44e56c4fdd578
Joey Cos <joeycosentino24@gmail.com>
2022-12-22 20:33:14 -0500

    establishing click events for buttons

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-class Calculator {
-    constructor(previousScreen, currentScreen) {
-        this.previousScreen = previousScreen;
-        this.currentScreen = currentScreen;
-    }
-
-    clear() {
-        this.currentScreen = '';
-        this.previousScreen = '';
-        this.operation = undefined;
-    }
-
-    delete() {
-
-    }
-
-    appendNumber(number) {
-
-    }
-
-    chooseOperation(operation) {
-
-    }
-
-    compute() {
-
-    }
-
-    updateDisplay() {
-
-    }
-}
-
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const allClearButton = document.querySelector('[data-all-clear]');
@@ -38,3 +5,17 @@ const equalsButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
 const previousScreen = document.querySelector('[data-previous]');
 const currentScreen = document.querySelector('[data-current]');
+
+function appearOnCurrentScreen() {
+    console.log('number has been clicked');
+}
+
+function allClear() {
+    console.log('all clear');
+}
+
+for (let number of numberButtons) {
+    number.addEventListener('click', appearOnCurrentScreen)
+}
+
+allClearButton.addEventListener('click', allClear)
