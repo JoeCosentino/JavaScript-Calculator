997702f36ce65af22d472645d9df56629475af9b
Joey Cos <joeycosentino24@gmail.com>
2022-12-24 14:15:19 -0500

    all clear button functional

--- index.js
+++ index.js
@@ -6,22 +6,25 @@ const deleteButton = document.querySelector('[data-delete]');
 const previousScreen = document.querySelector('[data-previous]');
 const currentScreen = document.querySelector('[data-current]');
 
-currentOutput = [];
+let currentOutput = [];
 
 function appearOnCurrentScreen() {
-    console.log('number has been clicked');
-    let x = this.value;
-    currentOutput.push(x);
-    console.log(currentOutput);
 
-    const currentOutputJoined = currentOutput.join('');
+    let buttonPressed = this.value;
+    
+    currentOutput.push(buttonPressed);
+    console.log(currentOutput);
+   
+    let currentOutputJoined = currentOutput.join('');
     console.log(currentOutputJoined);
 
     currentScreen.innerHTML = currentOutputJoined;
+    
 }
 
 function allClear() {
-    console.log('all clear');
+    currentScreen.innerHTML = '';
+    currentOutput = [];   
 }
 
 for (let number of numberButtons) {
