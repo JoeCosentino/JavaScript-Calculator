eb2e7350a8b76640061c519ed9deff4ca86ba0d2
Joey Cos <joeycosentino24@gmail.com>
2023-01-08 19:38:18 -0500

    reformatting js file

--- index.js
+++ index.js
@@ -1,43 +1,23 @@
-const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operation]');
-const allClearButton = document.querySelector('[data-all-clear]');
-const equalsButton = document.querySelector('[data-equals]');
-const deleteButton = document.querySelector('[data-delete]');
-const previousScreen = document.querySelector('[data-previous]');
-const currentScreen = document.querySelector('[data-current]');
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('#display');
 
-let currentOutput = [];
+let input = '';
+let operator = '';
+let operand1 = '';
+let operand2 = '';
 
-function appearOnCurrentScreen() {
-
-    let buttonPressed = this.value;
-    
-    currentOutput.push(buttonPressed);
-    console.log(currentOutput);
-   
-    let currentOutputJoined = currentOutput.join('');
-    console.log(currentOutputJoined);
-
-    currentScreen.innerHTML = currentOutputJoined;
-    
+for (let button of buttons) {
+  button.addEventListener('click', function() {
+    if (button.id === 'clear') {
+      input = '';
+      operator = '';
+      operand1 = '';
+      operand2 = '';
+      updateDisplay();
+    } 
+  });
 }
 
-function allClear() {
-    currentScreen.innerHTML = '';
-    currentOutput = [];   
+function updateDisplay() {
+  display.textContent = input;
 }
-
-function deleteInput() {
-    currentOutput.pop();
-    currentScreen.innerHTML = currentOutput.join('');
-}
-
-
-// buttons being pressed
-
-for (let number of numberButtons) {
-    number.addEventListener('click', appearOnCurrentScreen)
-}
-
-allClearButton.addEventListener('click', allClear);
-deleteButton.addEventListener('click', deleteInput);
